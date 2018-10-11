d63c24dd09a6f62b1e1a79213319d60c1d179550
MaxiPineker <maxi.pineker@gmail.com>
2018-10-11 09:11:10 +0200

    some problems in the code

--- assignment/pyramid_of_numbers.c
+++ assignment/pyramid_of_numbers.c
@@ -1,9 +1,9 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Class: <your class>
+ *				HTBLA-Leonding / Class: <2Dhif>
  * ---------------------------------------------------------
- * Exercise Number: 0
+ * Exercise Number: 3
  * Title:			Pyramid of Numbers
- * Author:			<your name>
+ * Author:			<MaxiPineker>
  * ----------------------------------------------------------
  * Description:
  * Calculates a pyramid of numbers, i.e., it multiplies a big
@@ -21,7 +21,7 @@
 *** @see MAX_DIGITS
 */
 struct BigInt {
-	/** number of digits of the big int. */
+	int digits[160];
 	int digits_count;
 
 	/** array of digits of big int. */
@@ -36,7 +36,9 @@ struct BigInt {
 *** @param *big_int The converted string now as BigInt.
 * @return The number of characters converted.
 */
+
 int strtobig_int(const char *str, int len, struct BigInt *big_int);
+
 
 /** print_big_int() prints a BigInt.
 *** @param *big_int The BigInt to be printed.
@@ -76,5 +78,41 @@ void copy_big_int(const struct BigInt *from, struct BigInt *to);
 */
 int main(int argc, char *argv[])
 {
+	char user_input[MAX_DIGITS];
+	printf("Pyramid of Numbers\n" );
+	printf("\n" );
+	printf("Please enter a number: " );
+	scanf("%s",user_input);
+	strtobig_int(*user_input, MAX_DIGITS, *big_int);
+
+	for (size_t i = 2; i < 10; i++) {
+		multiply(*big_int, i, *big_result);
+		printf(print_big_int(*big_int)" * %d = %d", i, &big_result);
+		big_int = big_result;
+	}
+	for (size_t i = 9; i < 1; i--) {
+		divide(*big_int, i, *big_result);
+		printf(print_big_int(*big_int)" * %d = %d", i, &big_result);
+		big_int = big_result;
+	}
 	return 0;
+}
+int strtobig_int(const char *str, int len, struct BigInt *big_int){
+	for (size_t i = 0; i < len; i++) {
+		if (str[i] >= '0' && str[i] <= '9') {
+			int temp = str[i];
+			big_int += temp;
+		}
+		return big_int;
+	}
+}
+
+void print_big_int(const struct BigInt *big_int){
+	printf("%d", &big_int);
+}
+void multiply(const struct BigInt *big_int, int factor, struct BigInt *big_result){
+	big_result = big_int * factor;
+}
+void divide(const struct BigInt *big_int, int divisor, struct BigInt *big_result){
+	big_result = big_int / divisor;
 }
